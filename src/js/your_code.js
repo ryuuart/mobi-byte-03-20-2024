5da2860f7dc688ac7b9796b1bd3e4f878800c2a7
Long Nguyen <me@long-nguyen.dev>
2024-03-20 20:35:05 -0500

    feat: add examples

--- src/js/your_code.js
+++ src/js/your_code.js
@@ -1,3 +1,49 @@
+// raf loop
+
+const cursorElement = document.getElementById("cursor");
+const offset = 0;
+let prevTime = 0;
+let currCursorPosition = { x: 0, y: 0 };
+let prevCursorPosition = { x: 0, y: 0 };
+function render(timestamp) {
+    const deltaTime = timestamp - prevTime;
+    prevTime = timestamp;
+    let alpha = deltaTime * 0.001;
+
+    const newCursorPos = {
+        x: lerp(prevCursorPosition.x, currCursorPosition.x, Math.min(alpha, 1)),
+        y: lerp(prevCursorPosition.y, currCursorPosition.y, Math.min(alpha, 1))
+    };
+    cursorElement.style.transform = `translate3d(${newCursorPos.x}px, ${newCursorPos.y}px, 0px)`
+    prevCursorPosition = newCursorPos;
+
+    requestAnimationFrame(render);
+}
+
+window.addEventListener("mousemove", function (ev) {
+    currCursorPosition = { x: ev.x, y: ev.y };
+});
+
 window.addEventListener("load", function () {
-    console.log("hi")
+    const elements = document.querySelectorAll(".face--box");
+    for (const element of elements) {
+        const keyframeEffects = [
+            {
+                transform: `translate(0, 0)`
+            },
+            {
+                transform: `translate(${Math.random() * 100}px, ${Math.random() * -500}px)`
+            }
+        ];
+
+        const keyframeOptions = {
+            duration: 1000,
+            fill: "forwards",
+            easing: "ease-in-out"
+        }
+
+        element.animate(keyframeEffects, keyframeOptions);
+    }
+
+    requestAnimationFrame(render);
 })
